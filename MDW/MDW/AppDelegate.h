67a19e208cd9378f0e248fe8d8bc6cb63cb126f8
michael <dr.mike.attia@gmail.com>
2017-04-12 11:06:29 +0200

    Initial Commit

---
+++ MDW/MDW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDW
+//
+//  Created by Michael on 4/12/17.
+//  Copyright © 2017 MAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MDW/MDW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MDW
+//
+//  Created by Michael on 4/12/17.
+//  Copyright © 2017 MAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
