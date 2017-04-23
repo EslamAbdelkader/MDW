906280d0e54173faad71ed8ec7aa03e3ac00ab71
JETS <jets@learniosfreecoms-mac.local>
2017-04-23 16:33:43 +0100

    ImageView Category

---
+++ MDW/UIImageView+ImageDownload.h
@@ -0,0 +1,14 @@
+//
+//  UIImageView+ImageDownload.h
+//  MDW
+//
+//  Created by JETS on 4/19/17.
+//  Copyright © 2017 MAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (ImageDownload)
+-(void) setSpeakerImageByURLString: (NSString *) url;
+-(void) setExhibitorImageByURLString: (NSString *) url;
+@end
