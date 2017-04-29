cae5a9a85800be4da1ee98fe757ced54f9274fbd
Rana <rana@Ranas-iMac.local>
2017-04-29 09:36:31 +0200

    user card/qr

--- MDW/MDW/ProfileContactsViewController.m
+++ MDW/MDW/ProfileContactsViewController.m
@@ -9,6 +9,7 @@
 #import "ProfileContactsViewController.h"
 #import "ProfileTabBarController.h"
 #import "AttendeeDTO.h"
+#import <ZXingObjC.h>
 
 @implementation ProfileContactsViewController{
     AttendeeDTO *attendee;
@@ -24,7 +25,20 @@
     _emailLbl.text = attendee.email;
     //_phoneLbl.text = attendee.
     
-    
+    NSString *encodedData=[ NSString stringWithFormat:@"BEGIN:VCARD\nVERSION:3.0\nN:%@;%@\nFN:\nORG:%@\nTITLE:%@\nTEL;CELL:%@\nEMAIL;WORK;INTERNET:%@\nURL:\nEND:VCARD", attendee.firstName, attendee.lastName, attendee.companyName, attendee.title, @"123", attendee.email];
+
+    NSError *error = nil;
+    CGImageRef image = nil;
+    ZXMultiFormatWriter *writer = [ZXMultiFormatWriter writer];
+    ZXBitMatrix* result = [writer encode:encodedData
+                                  format:kBarcodeFormatQRCode
+                                   width:500
+                                  height:500
+                                   error:&error];
+    if (result) {
+        image = [[ZXImage imageWithMatrix:result] cgimage];
+        [_qrImg setImage:[[UIImage alloc]initWithCGImage:image]];
+    }
 }
 
 @end
