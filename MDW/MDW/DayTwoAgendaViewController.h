ffbcb60ff186d4153b0dd54832af56a460a4710b
Rana <rana@Ranas-iMac.local>
2017-05-02 12:33:11 +0200

    refresh done, logout empties db

---
+++ MDW/MDW/DayTwoAgendaViewController.h
@@ -0,0 +1,13 @@
+//
+//  DayTwoAgendaViewController.h
+//  MDW
+//
+//  Created by Rana on 4/28/17.
+//  Copyright © 2017 MAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DayTwoAgendaViewController : UIViewController
+
+@end
